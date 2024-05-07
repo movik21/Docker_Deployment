5b293023ae82ef324c7e852b9a439abda2c2ef95
movik21 <moorvik1@students.zhaw.ch>
2024-05-07 12:45:29 +0200

    2nd update for docker image for deployment with Build-Trigger in Jenkins

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps folks - this app is now running on Docker :) <br> Running on http://${HOST}:${PORT}');
+res.send('Hello DevOps folks <3 <br> This app is build by Jenkins and is now running inside Docker :) <br> Docker Image: moorvik1/node-web-app  <br> Docker Container: moorvik1-web-app-container');
 });
 
 app.listen(PORT, HOST);
