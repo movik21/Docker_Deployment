2362387d13f7476ffc8e87e179bbd4f8db2343f0
movik21 <moorvik1@students.zhaw.ch>
2024-05-07 12:01:39 +0200

    First update for new version of docker image

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 'use strict';
 const express = require('express');
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello FS2024 DevOps Course!!!');
+res.send('Hello DevOps folks - this app is now running on Docker :) <br> Running on http://${HOST}:${PORT}');
 });
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
